537a1f38465ce9ccf49498db80ef13fa156fa92d
Valentin Minder <ValentinMinder@users.noreply.github.com>
2016-06-14 12:32:09 +0200

    edit user controller

--- Catstagram/app/controllers/users_controller.rb
+++ Catstagram/app/controllers/users_controller.rb
@@ -24,11 +24,6 @@ class UsersController < ApplicationController
     @cats = @user.cats
   end
 
-  # GET /users/new
-  def new
-    @user = User.new
-  end
-
   # GET /users/1/edit
   def edit
   end
@@ -75,22 +70,6 @@ class UsersController < ApplicationController
         end
       else
         format.html { render :ban }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # POST /users
-  # POST /users.json
-  def create
-    @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
